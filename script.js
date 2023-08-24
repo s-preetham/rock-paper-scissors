191379a82fb95fda6292a70cd76db55c8b615483
s-preetham <sunkupreetham95@gmail.com>
2023-08-24 20:08:05 +0530

    Add buttons,JS score keep

--- script.js
+++ script.js
@@ -11,57 +11,86 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+let userScore = 0;
+let CompScore = 0;
 
+
+
+function playRound(playerSelection) {
+    
     playerSelection = playerSelection.toLowerCase();
+   
+    let computerSelection = getComputerChoice();
     computerSelection = computerSelection.toLowerCase();
+    console.log(playerSelection,computerSelection)
 
+    
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
-            return "It's a draw!";
+            userScore += 1;
+            CompScore += 1;
         }
         else if (computerSelection === "paper") {
-            return "You lose!";
+            CompScore += 1;
         }
         else {
-            return "You win!";
+            userScore += 1;
         }
     }
     if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            return "You win!";
+            userScore += 1;
         }
         else if (computerSelection === "paper") {
-            return "It's a draw!";
+            userScore += 1;
+            CompScore += 1;
         }
         else {
-            return "You lose!";
+            CompScore += 1;
         }
     }
     if (playerSelection === "scissor") {
         if (computerSelection === "rock") {
-            return "You lose!";
+            CompScore += 1;
         }
         else if (computerSelection === "paper") {
-            return "You win!";
+            userScore += 1;
         }
         else {
-            return "It's a draw!";
+            userScore += 1;
+            CompScore += 1;
         }
     }
-}
-
-function game() {
-    for(let i=0;i<5;i++) {
-        const playerSelection = window.prompt("Enter your choice: ");
-        const computerSelection = getComputerChoice();
-        console.log("Your Selection:", playerSelection);
-        console.log("Computer's Selection:", computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
+    console.log("user score:",userScore)
+    console.log("Computer score:",CompScore)
+    if (userScore===5||CompScore===5) {
+        if (userScore===5 && CompScore===5) {
+            console.log("Draw!")
+        }
+        else if (CompScore===5) {
+            console.log("Computer Wins!")
+        }
+        else if (userScore===5){
+            console.log("User Wins!")
+        }
+        userScore = 0;
+        CompScore = 0;
     }
 }
 
-game()
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorBtn = document.getElementById("scissor");
+
+rVal = rockBtn.id;
+pVal = paperBtn.id;
+sVal = scissorBtn.id;
+
+rockBtn.addEventListener("click", (event) => console.log(playRound(rVal)));
+paperBtn.addEventListener("click", (event) => console.log(playRound(pVal)));
+scissorBtn.addEventListener("click", (event) => console.log(playRound(sVal)));
+
+
 
 
 
